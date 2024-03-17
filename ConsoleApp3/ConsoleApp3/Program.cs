d909b26affa836f9c94ab0ed0458b5d2a0c2fe1c
Shirkhan007 <bd7kdbz1b@code.edu.az>
2024-03-17 11:02:42 +0400

    methods

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Pold(1234));
+        }
+
+
+
+
+
+
+
+
+
+        public static int Pold(int nums)
+        {
+
+            
+            int b = 0;
+            while (nums > 0)
+            {
+                int qaliq = nums % 10;
+                b = (b * 10) + qaliq;
+                nums = nums / 10;
+              
+            }
+            return b;
+
+
+
+        }
+
+    }
+}
---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.Tracing;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = ("Shirkhan Asgarov");
+            Head(str);
+        }
+        public static void Head(string str)
+        {
+            string empty = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+
+                if (str[i]!=' ')
+                {
+                    empty += str[i];
+                }
+
+
+
+            }
+            Console.WriteLine(empty);
+          
+
+        }
+        
+        
+    }
+}
